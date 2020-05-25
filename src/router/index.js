5934679b7b8489110588e36b6c1a08c3f3f9f2a3
yangjiabinhome <754626536@qq.com>
2020-05-25 13:59:47 +0800

    serviceBar组件 首页轮播菜单及广告位实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,68 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../pages/home.vue'
-import Index from '../pages/index.vue'
-import Product from '../pages/product.vue'
-import Detail from '../pages/detail.vue'
+import Router from 'vue-router'
+import Home from '../pages/home'
+import Index from '../pages/index'
+Vue.use(Router);
 
-Vue.use(VueRouter)
-
-  const routes = [
+export default new Router({
+  routes:[
     {
-      path: '/',
-      name: 'home',
-      component: Home,
-      redirect: '/index',
+      path:'/',
+      name:'home',
+      component:Home,
+      redirect:'/index',
       children:[
         {
           path: '/index',
           name: 'index',
-          component: Index
-        },
-        {
+          component: Index,
+        }, {
           path: '/product/:id',
           name: 'product',
-          component: Product
-        },
-        {
+          component: () => import('../pages/product.vue')
+        }, {
           path: '/detail/:id',
           name: 'detail',
-          component: Detail
-        },
+          component: () => import('../pages/detail.vue')
+        }
       ]
     },
-  {
-    path: '/cart',
-    name: 'cart',
-    component: () => import('../pages/cart.vue')
-  },
-  {
-    path: '/order',
-    name: 'order',
-    component: () => import('../pages/order.vue'),
-    children:[
-      {
-        path: 'list',
-        name: 'order-list',
-        component: () => import('../pages/orderList.vue')
-      },
-      {
-        path: 'confirm',
-        name: 'order-confirm',
-        component: () => import('../pages/orderConfirm.vue')
-      },
-      {
-        path: 'pay',
-        name: 'order-pay',
-        component: () => import('../pages/orderPay.vue')
-      },
-      {
-        path: 'alipay',
-        name: 'alipay',
-        component: () => import('../pages/aliPay.vue')
-      }
-    ]
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/login.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../pages/cart.vue')
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import('../pages/order.vue'),
+      children:[
+        {
+          path: 'list',
+          name: 'order-list',
+          component: () => import('../pages/orderList.vue')
+        },
+        {
+          path: 'confirm',
+          name: 'order-confirm',
+          component: () => import('../pages/orderConfirm.vue')
+        },
+        {
+          path: 'pay',
+          name: 'order-pay',
+          component: () => import('../pages/orderPay.vue')
+        },
+        {
+          path: 'alipay',
+          name: 'alipay',
+          component: () => import('../pages/aliPay.vue')
+        }
+      ]
+    }
+  ]
+});
